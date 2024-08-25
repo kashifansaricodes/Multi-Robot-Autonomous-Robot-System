770b32915e24337c1ad344b124a9f9e9ebf5c09d
abhishek.avhad-at-623548713898 <abhishek@airgility.co>
2024-08-24 22:31:30 -0400

    Working Simple Controller

--- src/robot_urdf/launch/gazebo_sim.launch.py
+++ src/robot_urdf/launch/gazebo_sim.launch.py
@@ -2,38 +2,44 @@ import os
 from os import pathsep
 from ament_index_python.packages import get_package_share_directory, get_package_prefix
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, SetEnvironmentVariable, ExecuteProcess, RegisterEventHandler
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, SetEnvironmentVariable, RegisterEventHandler
 from launch.event_handlers import OnProcessExit
-from launch.substitutions import Command, LaunchConfiguration, FindExecutable
+from launch.substitutions import Command, LaunchConfiguration
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
 from launch_ros.parameter_descriptions import ParameterValue
 
 def generate_launch_description():
+    # Packages and directories
     robot_urdf = get_package_share_directory("robot_urdf")
     robot_controller = get_package_share_directory("robot_controller")
     robot_urdf_prefix = get_package_prefix("robot_urdf")
     gazebo_ros_dir = get_package_share_directory("gazebo_ros")
 
+    # Launch configuration
     model_arg = DeclareLaunchArgument(
         name="model", 
         default_value=os.path.join(robot_urdf, "urdf", "robot_urdf.urdf.xacro"),
         description="Absolute path to robot urdf file"
     )
 
+    # Set Gazebo model path
     model_path = os.path.join(robot_urdf, "models")
     model_path += pathsep + os.path.join(robot_urdf_prefix, "share")
     env_var = SetEnvironmentVariable("GAZEBO_MODEL_PATH", model_path)
 
+    # Robot description
     robot_description = ParameterValue(Command(["xacro ", LaunchConfiguration("model")]),
                                        value_type=str)
 
+    # Robot State Publisher
     robot_state_publisher_node = Node(
         package="robot_state_publisher",
         executable="robot_state_publisher",
         parameters=[{"robot_description": robot_description}]
     )
 
+    # Gazebo
     start_gazebo_server = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(gazebo_ros_dir, "launch", "gzserver.launch.py")
@@ -46,34 +52,60 @@ def generate_launch_description():
         )
     )
 
-    # Delete entity before spawning
-    delete_entity_cmd = ExecuteProcess(
-        cmd=['ros2', 'service', 'call', '/delete_entity', 'gazebo_msgs/DeleteEntity', '{"name": "robot_urdf"}'],
-        output='screen'
-    )
-
+    # Spawn robot
     spawn_robot = Node(
         package="gazebo_ros", 
         executable="spawn_entity.py",
         arguments=["-entity", "robot_urdf",
                    "-topic", "robot_description",
-                   "-x", "0", "-y", "0", "-z", "0.1"],  # Adjust spawn position as needed
+                   "-x", "0", "-y", "0", "-z", "0.1"],
         output="screen"
     )
 
+    # Load controllers
     controller_params_file = os.path.join(robot_controller, "config", "robot_controller.yaml")
 
-    # Load controllers
-    load_controllers = ExecuteProcess(
-        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
-             'joint_state_broadcaster'],
-        output='screen'
+    controller_manager = Node(
+        package="controller_manager",
+        executable="ros2_control_node",
+        parameters=[{"robot_description": robot_description},
+                    controller_params_file,
+                    {"use_sim_time": True}],
+        output="screen",
     )
 
-    load_velocity_controller = ExecuteProcess(
-        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
-             'simple_velocity_controller'],
-        output='screen'
+    load_joint_state_broadcaster = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_state_broadcaster", "--controller-manager", "/controller_manager"],
+    )
+
+    load_velocity_controller = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["simple_velocity_controller", "--controller-manager", "/controller_manager"],
+    )
+
+    # Delay loading of controllers
+    delay_controller_manager = RegisterEventHandler(
+        event_handler=OnProcessExit(
+            target_action=spawn_robot,
+            on_exit=[controller_manager],
+        )
+    )
+
+    delay_joint_state_broadcaster = RegisterEventHandler(
+        event_handler=OnProcessExit(
+            target_action=controller_manager,
+            on_exit=[load_joint_state_broadcaster],
+        )
+    )
+
+    delay_velocity_controller = RegisterEventHandler(
+        event_handler=OnProcessExit(
+            target_action=load_joint_state_broadcaster,
+            on_exit=[load_velocity_controller],
+        )
     )
 
     return LaunchDescription([
@@ -83,16 +115,7 @@ def generate_launch_description():
         start_gazebo_client,
         robot_state_publisher_node,
         spawn_robot,
-        RegisterEventHandler(
-            event_handler=OnProcessExit(
-                target_action=spawn_robot,
-                on_exit=[load_controllers],
-            )
-        ),
-        RegisterEventHandler(
-            event_handler=OnProcessExit(
-                target_action=load_controllers,
-                on_exit=[load_velocity_controller],
-            )
-        ),
+        delay_controller_manager,
+        delay_joint_state_broadcaster,
+        delay_velocity_controller
     ])
